13189c4bd1e9cd9774d78d39ad3c5d1d5a2ea492
Laryssa Freitas <laryssadara@hotmail.com>
2024-06-20 12:01:06 -0300

    Teste completo
    Tarefa 1 contendo a criação das tabelas e criação de dados, com a consulta SQL. E Tarefa 2 contendo a criação da arvore binaria, em C#, contando com dois cenarios diferentes.

---
+++ Tarefa 2/ParadigmaTeste/Program.cs
@@ -0,0 +1,24 @@
+﻿using ParadigmaTeste.Interfaces;
+using ParadigmaTeste.Services;
+using System;
+
+namespace ParadigmaTeste
+{
+  public class Program
+  {
+    private static void Main(string[] args)
+    {
+      IAtribuirValorArvore atribuirValorArvoreService = new AtribuirValorArvore();
+      IMontarArvore montarArvoreService = new MontarArvore();
+
+      int[] entradaCenarioUm = { 3, 2, 1, 6, 0, 5 };
+      int[] entradaCenarioDois = { 7, 5, 13, 9, 1, 6, 4 };
+
+      Console.WriteLine("arvore do cenario um:");
+      montarArvoreService.ImprimirArvore(atribuirValorArvoreService.GetArvoreProps(entradaCenarioUm));
+
+      Console.WriteLine("arvore do cenario dois:");
+      montarArvoreService.ImprimirArvore(atribuirValorArvoreService.GetArvoreProps(entradaCenarioDois));
+    }
+  }
+}
